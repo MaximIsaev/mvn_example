57b6a5f6a248871b2e51cdb06cae9a8269c3b1d6
Maxim Isaev <maxim_isaev1@epam.com>
2017-04-18 17:23:31 +0300

    add structure

---
+++ homemavenlesson/src/main/java/com/home/Home.java
@@ -0,0 +1,14 @@
+package com.home;
+
+import modone.ModuleOne;
+
+/**
+ * @author Isaev Maxim
+ *         date: 17.04.2017.
+ */
+public class Home {
+	public static void main(String[] args) {
+		ModuleOne modeOne = new ModuleOne();
+		modeOne.test();
+	}
+}
---
+++ moduleone/src/main/java/modone/ModuleOne.java
@@ -0,0 +1,11 @@
+package modone;
+
+/**
+ * @author Isaev Maxim
+ *         date: 17.04.2017.
+ */
+public class ModuleOne {
+	public void test() {
+		System.out.println("Test!");
+	}
+}
